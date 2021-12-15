329bae8d5fa1945c7c07dd140380b35c5837c4d5
John <jddhalogamer@gmail.com>
2021-12-15 17:00:02 -0600

    added AppNavbar component to app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <h2>Shopping List</h2>
     </div>
   );
 }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,16 @@
+import {
+    Collapse,
+    NavBar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+const AppNavbar = () => {
+
+}
+
+export default AppNavbar;
